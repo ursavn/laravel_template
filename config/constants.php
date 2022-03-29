224080c7b09824c1a9ef32bfb4626ac9fbed5c74
cuong <kopdcuo@gmail.com>
2022-03-29 10:27:11 +0700

    add css

---
+++ config/constants.php
@@ -0,0 +1,5 @@
+<?php
+if (!defined('ON')) define('ON', 1);
+if (!defined('OFF')) define('OFF', 0);
+if (!defined('FACEBOOK_LOGIN')) define('FACEBOOK_LOGIN', 0);
+if (!defined('GOOGLE_LOGIN')) define('GOOGLE_LOGIN', 0);
