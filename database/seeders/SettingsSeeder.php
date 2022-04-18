78102d2dd8435dc9327fbf7f550e8bc4edc7ef6e
cuongdv <cuong.dvan@ursa.vn>
2022-04-18 11:21:30 +0700

    fix redirect after register success

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -14,8 +14,8 @@ class SettingsSeeder extends Seeder
      */
     public function run()
     {
-        Setting::create(['name' => 'Login with facebook','status' => OFF]);
-        Setting::create(['name' => 'Login with google','status' => OFF]);
-        Setting::create(['name' => 'Verify email address','status' => ON]);
+        Setting::create(['name' => 'Login with facebook', 'short_name' => 'facebook', 'status' => OFF]);
+        Setting::create(['name' => 'Login with google', 'short_name' => 'google', 'status' => OFF]);
+        Setting::create(['name' => 'Verify email address', 'short_name' => 'verify', 'status' => OFF]);
     }
 }
