0afb67dd1a662e26b86cce181726cf429602ca89
Van Pham <van.pham@ursa.vn>
2022-04-21 11:13:14 +0700

    update guard name

--- app/Http/Requests/Api/UsersRequest/CreateRequest.php
+++ app/Http/Requests/Api/UsersRequest/CreateRequest.php
@@ -4,7 +4,9 @@ namespace App\Http\Requests\Api\UsersRequest;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
+use Spatie\Permission\Models\Role;
 use Symfony\Component\HttpFoundation\Response;
 
 class CreateRequest extends FormRequest
@@ -26,10 +28,17 @@ class CreateRequest extends FormRequest
      */
     public function rules()
     {
+        $roles = Role::all()->toArray();
+
+        $roles = array_map(function(array $value): string {
+            return $value['name'];
+        }, $roles);
+
         return [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'role' => ['required', Rule::in($roles)],
         ];
     }
 
@@ -43,6 +52,7 @@ class CreateRequest extends FormRequest
     protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator)
     {
         $errors = (new ValidationException($validator))->errors();
+
         throw new HttpResponseException(response()->json(
             [
                 'error' => $errors,
